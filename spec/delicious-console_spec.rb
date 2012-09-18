9116dbadb6ce591fabee738f089ec23cecd8cc53
Mateus Prado <mateus@mateusprado.com>
2012-09-18 01:28:29 -0300

    fix spec to delicious-console

--- spec/delicious-console_spec.rb
+++ spec/delicious-console_spec.rb
@@ -1,8 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "The Delicious Console" do
-  context "when used by someone" do
-    it "should be work fine" do
+  context "when get all posts" do
+    it "should be show all bookmarks url" do
 			Delicious::Console.get_all_posts
 		end
   end
